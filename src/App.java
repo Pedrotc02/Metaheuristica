83ff19bd09acf89a06cf1ca397e0b1fc874733a1
Jaime Zafra <32645705+ElZafro@users.noreply.github.com>
2023-10-24 16:29:47 +0200

    medir tiempo de ejecución

--- src/App.java
+++ src/App.java
@@ -6,7 +6,13 @@ public class App {
     public static void main(String[] args) {
         try {
             Config config = new Config();
+
+            var start = System.nanoTime();
             Solution solution = config.Solve();
+            var end = System.nanoTime();
+
+            System.out.format("Tiempo de ejecución: %.3fms", (end - start) * 1e-6);
+
             Printer.printSolution("Solucion final", solution);
         } catch (Exception e) {
             Printer.printException(e);
